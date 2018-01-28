d2c1f06592fadf8a9f6148ef9a8732afaf80b0a3
paul (local) <ps@artspaces.net>
2018-01-27 17:33:35 -0700

    find host address

--- api/v1/console/index.js
+++ api/v1/console/index.js
@@ -2,10 +2,10 @@ exports.module = process.console = require('./src/console.js').module;
 
 process.app.get('/v1/console', function(request, response) {
 
-    var ip = request.header('x-forwarded-for') || request.connection.remoteAddress;
+    var domain = request.protocol + '://' + request.get('host');
 
     response.writeHead(200);
-    response.write("Hello API console. My IP is: "+ip);
+    response.write("Hello API console. My IP is: "+domain);
     response.end();
 
 });
